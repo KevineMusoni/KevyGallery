44fad3daf5a8f9d59be17c926aacd3b075c34c62
Kevine Musoni <kevinenicky@gmail.com>
2018-12-08 19:08:11 +0300

    homepage link

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
+    # landing page
     url('^$',views.gallery,name = 'gallery'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.image,name='image')
