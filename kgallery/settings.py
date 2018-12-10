08f62d184ec9a8b586bbca53eb0598003e8bb729
Kevine Musoni <kevinenicky@gmail.com>
2018-12-10 05:57:44 +0300

    debug set to false

--- kgallery/settings.py
+++ kgallery/settings.py
@@ -24,7 +24,7 @@ MODE = config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 SITE_ID=1
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
